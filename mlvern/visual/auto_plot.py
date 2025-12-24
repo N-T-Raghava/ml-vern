ff16614d4c636d8c571a70f145cab9f377287612
Tanmai Raghava <tanmairaghav3836@gmail.com>
2025-12-24 23:11:40 +0530

    configured line length to lint

--- mlvern/visual/auto_plot.py
+++ mlvern/visual/auto_plot.py
@@ -1,10 +1,13 @@
 import os
 
 import matplotlib.pyplot as plt
-from sklearn.metrics import ConfusionMatrixDisplay
-from sklearn.metrics import RocCurveDisplay
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import roc_curve
+from sklearn.metrics import (
+    ConfusionMatrixDisplay,
+    RocCurveDisplay,
+    confusion_matrix,
+    roc_curve,
+)
+
 
 def auto_plot(task, y_true=None, y_pred=None, y_prob=None, output_dir="."):
     os.makedirs(output_dir, exist_ok=True)
